66c9ea15b0b60203a7bfc76ee66fddecff90aea9
Rafael Gomes Gonzaga <rgomesgonzaga@gmail.com>
2022-07-26 17:28:44 -0300

    salvar

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,6 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
-  reporterOptions: {
-    reportDir: "cypress/report/mochawesome-report",
-    overwrite: true,
-    html: true,
-    json: false,
-    timestamp: "yyyymmdd_HHMMss"
-  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
