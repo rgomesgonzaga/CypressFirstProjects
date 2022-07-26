474e8dbc5488f938b799007cb334b2d0821a0d66
Rafael Gomes Gonzaga <rgomesgonzaga@gmail.com>
2022-07-26 15:44:33 -0300

    ajustando arquivo de configuração do cypress para utilizar o mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -1,20 +1,22 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      
     },
   },
+<<<<<<< HEAD
 });
-
-{
-  "reporter": "mochawesome",
-  "reporterOptions": {
-  "reportDir": "cypress/report/mochawesome-report",
-  "overwrite": true,
-  "html": true,
-  "json": false,
-  "timestamp": "mmddyyyy_HHMMss"
-  }    
-}
+=======
+});
+>>>>>>> parent of 2076b74 (Inserindo configurações do mochawesome no arquivo de configuração do cypress)
