2076b746fb5a8db95614769daacf7abffdc4fb9e
Rafael Gomes Gonzaga <rgomesgonzaga@gmail.com>
2022-07-26 15:15:28 -0300

    Inserindo configurações do mochawesome no arquivo de configuração do cypress

--- cypress.config.js
+++ cypress.config.js
@@ -7,3 +7,14 @@ module.exports = defineConfig({
     },
   },
 });
+
+{
+  "reporter": "mochawesome",
+  "reporterOptions": {
+  "reportDir": "cypress/report/mochawesome-report",
+  "overwrite": true,
+  "html": true,
+  "json": false,
+  "timestamp": "mmddyyyy_HHMMss"
+  }    
+}
