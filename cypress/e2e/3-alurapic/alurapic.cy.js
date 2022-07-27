ab3532623b9c32a82fcf07fd10e6c788a49d0ea5
Rafael Gomes Gonzaga <rgomesgonzaga@gmail.com>
2022-07-26 22:46:27 -0300

    Adding two more test itens. Login valid user and invalid user

--- cypress/e2e/3-alurapic/alurapic.cy.js
+++ cypress/e2e/3-alurapic/alurapic.cy.js
@@ -53,4 +53,19 @@ describe('Testa o registro e login de usuários no alura pic', () => {
         cy.contains('ap-vmessage', 'Must be lower case').should('be.visible');
     })
 
+    it('fazer login de usuário válido', () => {
+        cy.get('input[formcontrolname="userName"]').type('flavio');
+        cy.get('input[formcontrolname="password"]').type('123');
+        cy.get('button[type="submit"]').click();
+        cy.contains('a', '(Logout)').should('be.visible');
+    })
+
+    it('fazer login de usuário inválido', () => {
+        cy.get('input[formcontrolname="userName"]').type('rafael');
+        cy.get('input[formcontrolname="password"]').type('1234');
+        cy.get('button[type="submit"]').click();
+        cy.on('window:alert', (str) => {
+            expect(str).to.equal('Invalid user name or password')
+        })
+    })    
 })
