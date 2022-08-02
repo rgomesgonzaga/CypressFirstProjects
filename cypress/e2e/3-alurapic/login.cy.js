51a774aa7647918017aed813891639ddeb80b105
Rafael Gomes Gonzaga <rgomesgonzaga@gmail.com>
2022-08-01 23:17:52 -0300

    Simular uma resposta com código diferente de 200 a uma requisição a API usando um stub

--- cypress/e2e/3-alurapic/login.cy.js
+++ cypress/e2e/3-alurapic/login.cy.js
@@ -2,10 +2,15 @@ describe('Testa o login de usuários no alura pic', () => {
 
     beforeEach(() => { 
         cy.visit('/') 
+
+        cy.intercept('POST', 'https//alurapic.herokuapp.com/user/login', {
+            statusCode: 400
+        }).as('stubPOST')
     })
 
     it('fazer login de usuário válido', () => {
         cy.login(Cypress.env('userName'), Cypress.env('password'));
+        cy.wait('@stubPOST')
         cy.contains('a', '(Logout)').should('be.visible');
     })
 
