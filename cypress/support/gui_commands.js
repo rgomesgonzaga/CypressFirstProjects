350f73eb3b33d016e4e5858cdd8372cbac6ed3b4
Rafael Gomes Gonzaga <rgomesgonzaga@gmail.com>
2022-08-01 23:00:37 -0300

    Escondendo senha na página de execução do Cypress

--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -1,6 +1,6 @@
 Cypress.Commands.add('login', (nome, senha) => {
     cy.get('input[formcontrolname="userName"]').type(nome);
-    cy.get('input[formcontrolname="password"]').type(senha);
+    cy.get('input[formcontrolname="password"]').type(senha, {log: false});
     cy.get('button[type="submit"]').click();
 })
 
@@ -10,6 +10,6 @@ Cypress.Commands.add('register', (email, fullName, userName, password) => {
     cy.get('input[formcontrolname="email"]').type(email);
     cy.get('input[formcontrolname="fullName"]').type(fullName);
     cy.get('input[formcontrolname="userName"]').type(userName);
-    cy.get('input[formcontrolname="password"]').type(password);
+    cy.get('input[formcontrolname="password"]').type(password, {log: false});
     cy.contains('button','Register').click();
 })
